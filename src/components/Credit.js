87188e3c69d051494a99f44581ebe0f67f3005a6
VAmanJain <vaman.jain17@gmail.com>
2023-10-18 19:13:38 +0530

    first

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit" style={{color:"black"}}>
+    <p>
+      Coded by
+      <a href="https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAAEE7ijUBsI3ZRNOShV-HhEuTyZMVdfeSPSo&keywords=ankit%20sharma&origin=RICH_QUERY_SUGGESTION&position=0&searchId=97c1680f-3244-4da1-8623-ae543335c241&sid=r77&spellCorrectionEnabled=false" target="_blank"
+        > ANKIT SHARAMA</a
+      >
+    </p>
+  </div>
+  );
+}
